b040a54ba0a9b40edbf2f733759a4ee03b754c5c
Spencer Kulbacki <sometimestanley@gmail.com>
2017-02-05 15:31:40 -0500

    Baked AO texture and exported Body Mesh

---
+++ vehicles/Midget/materials.cs
@@ -0,0 +1,11 @@
+singleton Material(MG_Body)
+{
+   mapTo = "MG_Body";
+   diffuseMap[0] = "AO_bake.png";
+   specularPower[0] = "15";
+   useAnisotropic[0] = "1";
+   castShadows = "1";
+   translucent = "0";
+   alphaTest = "0";
+   alphaRef = "0";
+};
