a5fb4979d17fa338ac40c237229a7f687fe41d7a
Spencer Kulbacki <sometimestanley@gmail.com>
2017-02-05 17:28:33 -0500

    missing texture issue...

--- vehicles/Midget/materials.cs
+++ vehicles/Midget/materials.cs
@@ -1,11 +1,17 @@
-singleton Material(MG_Body)
+singleton Material(material1)
 {
-   mapTo = "MG_Body";
-   diffuseMap[0] = "AO_bake.png";
+   mapTo = "body";	//the name of the mesh in blender not the DAE filename
+   diffuseMap[0] = "vehicles/Midget/AO_bake.dds";
    specularPower[0] = "15";
-   useAnisotropic[0] = "1";
-   castShadows = "1";
-   translucent = "0";
-   alphaTest = "0";
+   useAnisotropic[0] = "1";  
+   
+   diffuseColor[0] = "1 1 1 1";
+   
+   castShadows = "0";
+   translucent = "1";
+   alphaTest = "1";
    alphaRef = "0";
+   dynamicCubemap = true; //cubemap = "BNG_Sky_02_cubemap";
+   materialTag0 = "beamng"; materialTag1 = "vehicle";
+	
 };
